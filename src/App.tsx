488a8a0d60497c9a602d3718f0a0df59e95db045
gorkavillar <gorka@gorkavillar.com>
2023-02-17 13:19:42 +0100

    💅🏻 Pintando nuestras vistas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import EventosRaton from "./components/eventos/EventosRaton"
-import EventosTeclado from "./components/eventos/EventosTeclado"
-import IntroEventos from "./components/eventos/IntroEventos"
+/* eslint-disable @typescript-eslint/no-unused-vars, no-unreachable */
+import "./App.css"
+import { Home, Contact, Admin, Login } from "views"
+
+const routes = {
+    home: <Home />,
+    contact: <Contact />,
+    admin: <Admin />,
+    login: <Login />,
+}
 
 function App() {
+    let currentRoute: string
+
+    // return routes[currentRoute]
+
+    // switch (currentRoute) {
+    //     case "home": return <Home />
+    //     case "contact": return <Contact />
+    //     case "admin": return <Admin />
+    //     case "login": return <Login />
+    // }
+
+    // if (currentRoute === "home") return <Home />
+    // if (currentRoute === "contact") return <Contact />
+    // if (currentRoute === "admin") return <Admin />
+    // if (currentRoute === "login") return <Login />
+    currentRoute = "login"
+    const mostrar = true
     return (
-        <div>
-            {/* <IntroEventos /> */}
-            {/* <EventosTeclado /> */}
-            <EventosRaton />
-        </div>
+        <>
+            {mostrar && (
+                <div>
+                    {currentRoute === "home" && <Home />}
+                    {currentRoute === "contact" && <Contact />}
+                    {currentRoute === "admin" && <Admin />}
+                    {currentRoute === "login" && <Login />}
+                    {/* {currentRoute === "home"
+                ? "Hola mundo desde home"
+                : "No estoy en la home"} */}
+                </div>
+            )}
+        </>
     )
 }
 
---
+++ src/views/index.ts
@@ -0,0 +1,6 @@
+import Admin from "./Admin"
+import Contact from "./Contact"
+import Home from "./Home"
+import Login from "./Login"
+
+export { Admin, Contact, Home, Login }
