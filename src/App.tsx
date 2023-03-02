098b73ebca2cfafd6dbcc1fa55562da725742d05
gorkavillar <gorka@gorkavillar.com>
2023-03-02 09:51:56 +0100

    🩹 Tema 11 - Bootstrap con CDN

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, no-unreachable, jsx-a11y/anchor-is-valid */
-import Home from "views/Home"
-
+import Bootstrap from "views/Bootstrap"
 import "./App.scss"
-import SettingsView from "views/SettingsView"
-import AppContextProvider from "contexts/AppContextProvider"
-import ContadorContextProvider from "contexts/ContadorContextProvider"
-import Contador from "views/Contador"
 
 function App() {
     return (
-        <AppContextProvider>
-            <Home />
-            <SettingsView />
-            <ContadorContextProvider>
-                <Contador />
-            </ContadorContextProvider>
-        </AppContextProvider>
+        <div>
+            <Bootstrap />
+        </div>
     )
 }
 
