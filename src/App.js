6949c9010bfe69d3e7228e36173ffe46d6409bc8
gorkavillar <gorka@gorkavillar.com>
2023-03-03 12:59:04 +0100

    🕷️ Tema 14 - Webpack, babel, eslint, etc...

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+// App es un componente funcional que va a mostrar...
+const App = () => {
+  return (
+    <div>
+      <h1>Bienvenido a App con React desde cero</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/functions.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns La suma de a + b
+ */
+const suma = (a, b) => a + b
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @returns La multiplicación de a * b
+ */
+const multiplica = (a, b) => a * b
+
+module.exports = { suma, multiplica }
